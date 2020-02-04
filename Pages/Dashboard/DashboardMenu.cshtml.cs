109eb4ea167f7ed4c6808528105f57a70ee777bd
Toby Niesser <40204944+TobyNiesser@users.noreply.github.com>
2020-02-04 15:58:33 +0000

    Update 04/02/2020 16:00PM

---
+++ Pages/Dashboard/DashboardMenu.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FACILITIES
+{
+    public class DashboardMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}

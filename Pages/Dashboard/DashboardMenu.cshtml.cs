4a9f9a422e1c47498c4262736ec8b7b332d36e85
Toby Niesser <40204944+TobyNiesser@users.noreply.github.com>
2020-02-12 14:29:23 +0000

    Update 12.02.2020 14:30PM

--- Pages/Dashboard/DashboardMenu.cshtml.cs
+++ Pages/Dashboard/DashboardMenu.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FACILITIES.Models;
 
@@ -11,9 +12,46 @@ namespace FACILITIES
 {
     public class DashboardMenuModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private readonly FACILITIES.Models.FACILITIESContext _context;
+
+        public DashboardMenuModel(FACILITIES.Models.FACILITIESContext context)
+        {
+            _context = context;
+        }
+        //public void OnGet()
+        //{
+
+        //}
+
+        public IActionResult OnGet()
+        {
+            ViewData["CompanyID"] = new SelectList(_context.Company, "CompanyID", "Name");
+            ViewData["FrequencyID"] = new SelectList(_context.Frequency, "FrequencyID", "FrequencyAmount");
+            ViewData["ItemID"] = new SelectList(_context.Item, "ItemID", "ItemName");
+            ViewData["OfficeID"] = new SelectList(_context.Office, "OfficeID", "Name");
+            ViewData["ResponsibilityID"] = new SelectList(_context.Responsibility, "ResponsibilityID", "Name");
+            ViewData["StatusID"] = new SelectList(_context.Status, "StatusID", "StatusIndicator");
+            return Page();
+        }
+
+
+        [BindProperty]
+        public Setting Setting { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Setting.Add(Setting);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
+
+
 }

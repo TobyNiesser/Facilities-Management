8874c7de5848cba6c658c0cae3094e98b21e178f
Toby Niesser <40204944+TobyNiesser@users.noreply.github.com>
2020-02-21 13:53:04 +0000

    UPDATE 21/02/2020 14:00PM

--- Pages/Dashboard/DashboardMenu.cshtml.cs
+++ Pages/Dashboard/DashboardMenu.cshtml.cs
@@ -20,7 +20,7 @@ namespace FACILITIES
             _context = context;
         }
         
-        //ADD NEW SYSTEM - SNIPPET -- START
+        
         public IActionResult OnGet()
         {
             ViewData["CompanyID"] = new SelectList(_context.Company, "CompanyID", "Name");
@@ -31,7 +31,7 @@ namespace FACILITIES
             ViewData["StatusID"] = new SelectList(_context.Status, "StatusID", "StatusIndicator");
             return Page();
         }
-        //ADD NEW SYSTEM - SNIPPET -- END
+        
 
         [BindProperty]
         public Setting Setting { get; set; }

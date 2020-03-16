78694ef4f3f0942aa55ef8c11ddc6358f9717f7b
Toby Niesser <40204944+TobyNiesser@users.noreply.github.com>
2020-03-16 16:37:20 +0000

    UPDATE 16/03/2020 16:30PM

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FACILITIES.Areas.Identity.Data;
+using FACILITIES.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FACILITIES.Areas.Identity.IdentityHostingStartup))]
+namespace FACILITIES.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UsersContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UsersContextConnection")));
+
+                services.AddDefaultIdentity<FACILITIESUser>()
+                    .AddEntityFrameworkStores<UsersContext>();
+            });
+        }
+    }
+}
